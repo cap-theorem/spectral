a6a4e0621fe2d58298d54f0ebba16ec590e876cb
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-09-04 14:36:36 -0500

    feat: barebones discovery actor

---
+++ internal/discovery/actor.go
@@ -0,0 +1,32 @@
+package discovery
+
+import "context"
+
+type Actor struct {
+	inbox <-chan struct{}
+}
+
+func NewActor() *Actor {
+	return &Actor{
+		inbox: make(<-chan struct{}),
+	}
+}
+
+func (a *Actor) Inbox() <-chan struct{} {
+	return a.inbox
+}
+
+func (a *Actor) Run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case msg := <-a.inbox:
+			a.handle(msg)
+		}
+	}
+}
+
+func (a *Actor) handle(x struct{}) {
+	// Do something here
+}
