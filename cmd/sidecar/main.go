3ff932e2853d8f3794c09e3cab39a0aa820cb55b
AlexanderWangY <alexander.yisu.wang@outlook.com>
2026-09-04 22:39:53 -0500

    chore: update main to remove old timer

--- cmd/sidecar/main.go
+++ cmd/sidecar/main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
-
-	"github.com/cap-theorem/spectral/internal/clock"
 )
 
 func main() {
-	timer := clock.NewTimer()
-	tick := 0
-
-	go timer.Run(context.Background())
-
-	for range timer.C() {
-		tick++
-		fmt.Printf("Tick: %d\n", tick)
-	}
+	fmt.Println("Welcome to spectral")
 }
